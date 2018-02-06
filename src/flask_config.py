54983241f593169210da7e56d3963d99b08a276e
Tom Evans <TomosAMEvans@gmail.com>
2018-02-06 15:47:30 +0000

    turned off sql tracking modifications

--- src/flask_config.py
+++ src/flask_config.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY']              = 'helloworld'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database.db'
+app.config['SECRET_KEY']                     = 'helloworld'
+app.config['SQLALCHEMY_DATABASE_URI']        = 'sqlite:///./database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db = SQLAlchemy(app)
