957f08c20d7db3c5ad8b43a8a1f161ff7de3106c
tpbadger <tombadger12@gmail.com>
2018-02-12 21:57:29 +0000

    added email validator extension that checks for UOB email during sign up

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # OctoLink
-A web based system for connecting and controlling a printer farm remotely. Developed primarily for the University of Bristol's Backspace and it's 6 Ultimaker 2+
+A web based system for connecting and controlling a printer farm remotely. Developed primarily for the University of Bristol's Hackspace and it's 6 Ultimaker 2+
--- src/flask_config.py
+++ src/flask_config.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful    import Api
+from flask_restful import Api
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY']                     = 'helloworld'
-app.config['SQLALCHEMY_DATABASE_URI']        = 'sqlite:///./database.db'
+app.config['SECRET_KEY'] = 'helloworld'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
