3885c3359e8b26c5f67505bca67582d25642b094
Tom Evans <TomosAMEvans@gmail.com>
2018-02-13 22:20:21 +0000

    started job

--- src/jobs.py
+++ src/jobs.py
@@ -1,2 +1,29 @@
+from users    import User
+
 class Job():
-    pass
+    """
+    Represents a Job.
+    """
+    filename       : str
+    user           : User
+    colour         : str
+    material       : str
+    location       : str
+    time_remaining : str
+
+
+    def __init__(self, filename,
+                       user,
+                       colour,
+                       material,
+                       location=None,
+                       time_remaining=None,
+                       printing_on=None):
+
+        self.filename       = filename
+        self.colour         = colour
+        self.material       = material
+        self.user           = user
+        self.location       = location
+        self.time_remaining = time_remaining
+        self.printing_on    = printing_on
